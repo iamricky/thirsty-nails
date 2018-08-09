9e74c5cf519e00dbfae7b58812ff23c9e8795d22
Dave Kozma <dave@workframe.com>
2018-08-09 12:19:02 -0400

    Added secondary component

--- src/cljs_for_js_devs/app.cljs
+++ src/cljs_for_js_devs/app.cljs
@@ -2,13 +2,27 @@
     (:require [reagent.core :as r]
               [stylefy.core :refer [use-style]]))
 
+(def img-style
+  {:margin-top "20px"})
+
+;; Dog GIF react component
+
+(defn dog-image []
+  [:img (use-style img-style
+          {:src "https://i.imgur.com/YCnyVDq.gif"})])
+
 (def header-style
   {:font-size "24px"
    :font-weight "bold"
    :padding "0"
    :margin "0"})
 
+;; Main react component
+
 (defn root []
-  [:h1 (use-style header-style)
-   "Hello, world!"])
+  [:div
+    [:h1 (use-style header-style)
+     "Hello!"]
+    [dog-image]])
+
 
