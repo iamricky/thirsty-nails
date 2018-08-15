f24c9cb3bff742522b7b654d5b68ad3498242872
Unknown <rickymuldrew@gmail.com>
2018-08-15 11:15:33 -0400

    rm boilerplate code

--- src/cljs_for_js_devs/app.cljs
+++ src/cljs_for_js_devs/app.cljs
@@ -1,28 +1,8 @@
 (ns cljs-for-js-devs.app
     (:require [reagent.core :as r]
-              [stylefy.core :refer [use-style]]))
-
-(def img-style
-  {:margin-top "20px"})
-
-;; Dog GIF react component
-
-(defn dog-image []
-  [:img (use-style img-style
-          {:src "https://i.imgur.com/YCnyVDq.gif"})])
-
-(def header-style
-  {:font-size "24px"
-   :font-weight "bold"
-   :padding "0"
-   :margin "0"})
-
-;; Main react component
+              [stylefy.core :refer [use-style]]
+              [cljs-for-js-devs.chatroom :refer [chatroom]]))
 
 (defn root []
   [:div
-    [:h1 (use-style header-style)
-     "Hello!"]
-    [dog-image]])
-
-
+    [chatroom]])
