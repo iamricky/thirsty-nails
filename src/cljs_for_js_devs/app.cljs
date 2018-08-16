db8d570c450b9071f70dd6b58ac8930f3b6930f8
Unknown <rickymuldrew@gmail.com>
2018-08-16 14:54:39 -0400

    fmt file; create app-state

--- src/cljs_for_js_devs/app.cljs
+++ src/cljs_for_js_devs/app.cljs
@@ -1,8 +1,23 @@
 (ns cljs-for-js-devs.app
-    (:require [reagent.core :as r]
-              [stylefy.core :refer [use-style]]
-              [cljs-for-js-devs.chatroom :refer [chatroom]]))
+  (:require [reagent.core :as r]
+            [stylefy.core :refer [use-style]]
+            [cljs-for-js-devs.chatroom :refer [chatroom]]))
+
+(def user-model {:current-user nil
+                 :id nil
+                 :firstname "Andre"
+                 :lastname "Wheeler"
+                 :name nil
+                 :avatarId nil})
+
+(def room-model {:id nil
+                 :users nil
+                 :created nil
+                 :unread nil
+                 :total nil})
+
+(defonce app-state (r/atom {:user user-model :room room-model}))
 
 (defn root []
   [:div
-    [chatroom]])
+   [chatroom @app-state]])
