dab4e67a5bcc1b4b8fb82e79a8edd37d9daa2865
Unknown <rickymuldrew@gmail.com>
2018-08-16 14:56:04 -0400

    refactor implementation for chat submit

--- src/cljs_for_js_devs/chatroom.cljs
+++ src/cljs_for_js_devs/chatroom.cljs
@@ -1,10 +1,18 @@
 (ns cljs-for-js-devs.chatroom
-    (:require [reagent.core :as r]
-              [stylefy.core :refer [use-style]]
-              [cljs-for-js-devs.messages :refer [message form]]))
+  (:require [reagent.core :as r]
+            [stylefy.core :refer [use-style]]
+            [cljs-for-js-devs.chatroom-styles :refer [chatroom-style form-style input-style button-style]]
+            [cljs-for-js-devs.messages :refer [message]]))
 
-(def chatroom-style
-  { :background "#fff" :box-shadow "5px 5px 0px 0px rgba(0,0,0,0.25)" :min-height "190px" :width "450px" :position "relative"})
+(defn submit-chat []
+  (let [state (atom {:value ""})]
+    (fn [] [:form (use-style form-style) 
+      [:label [:input (use-style input-style {:id :chat
+                                              :type :text
+                                              :value (:value @state)
+                                              :placeholder "Enter Reply Here"})]]
+      [:button (use-style button-style {:type "submit" :on-click (fn [e] (.preventDefault e))}) "reply"]])))
 
 (defn chatroom []
-  [:section (use-style chatroom-style) [message] [form]])
+  [:main (use-style chatroom-style)
+  [:section [message]] [submit-chat]])
--- src/cljs_for_js_devs/messages.cljs
+++ src/cljs_for_js_devs/messages.cljs
@@ -1,25 +1,12 @@
 (ns cljs-for-js-devs.messages
-    (:require [reagent.core :as r]
-              [stylefy.core :refer [use-style]]))
+  (:require [reagent.core :as r]
+            [stylefy.core :refer [use-style]]
+            [cljs-for-js-devs.messages-styles :refer [message-style username-style timestamp-style text-style]]))
 
-(def message-style { :position "absolute" :bottom "0" :width "100%"})
-
-(defn message [] 
-  [:article (use-style message-style) "testing"])
-
-(def button-style
-  { :border "none" :background "#00c19e" :color "#fff" :padding "15px 0" :text-transform "capitalize" :display "block" :width "100%"})
-
-(defn button []
-  [:button (use-style button-style { :type "submit" }) "reply"])
-
-(def input-style
-  { :margin "20px auto" :padding "10px" :display "block" :width "90%" :box-sizing "border-box"})
-
-(defn input []
-  [:label [:input (use-style input-style { :type "text" :placeholder "Enter Reply Here"})]])
-
-(def form-style { :position "absolute" :bottom "0" :width "100%"})
-
-(defn form []
-  [:form (use-style form-style) [input] [button]])
+(defn message []
+  [:article (use-style message-style)
+   [:header
+    [:span (use-style username-style) "Andre Wheeler"]
+    [:span (use-style timestamp-style) (str (js/Date.))]]
+   [:p (use-style text-style) "We have to make sure that we have all the punch list items accounted for. 
+      Cory Robinson I want you to take the lead on this."]])
