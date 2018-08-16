11744a2326d013c10586da9911379c619067adcf
Unknown <rickymuldrew@gmail.com>
2018-08-16 14:55:10 -0400

    mv styles to separate namespaces

---
+++ src/cljs_for_js_devs/chatroom-styles.cljs
@@ -0,0 +1,30 @@
+(ns cljs-for-js-devs.chatroom-styles)
+
+(def button-style
+  {:border "none"
+   :background "#00c19e"
+   :color "#fff"
+   :padding "15px 0"
+   :text-transform "capitalize"
+   :display "block"
+   :width "100%"
+   :outline "none"
+   :font "bold 14px Lato, sans-serif"})
+
+(def input-style
+  {:margin "20px auto"
+   :padding "10px"
+   :display "block"
+   :width "90%"
+   :box-sizing "border-box"
+   :outline "none"})
+
+(def form-style
+  {:bottom "0"
+   :width "100%"})
+
+(def chatroom-style
+  {:background "#fff"
+   :box-shadow "5px 5px 0px 0px rgba(0,0,0,0.25)"
+   :width "450px"
+   :position "relative"})
---
+++ src/cljs_for_js_devs/messages-styles.cljs
@@ -0,0 +1,23 @@
+(ns cljs-for-js-devs.messages-styles)
+
+(def message-style
+  {:color "#121212"
+   :margin "0 auto"
+   :padding "20px 0 0"
+   :display "block"
+   :width "90%"
+   :font-size "12px"})
+
+(def username-style
+  {:font "bold 12px/15px Lato, sans-serif"
+   :margin-right "10px"})
+
+(def timestamp-style
+  {:font "normal 12px/15px Lato, sans-serif"
+   :color "#bdbec0"
+   :border-left "1px solid #bdbec0"
+   :margin-right "10px"
+   :padding-left "10px"})
+
+(def text-style
+  {:font "normal 12px/15px Lato, sans-serif"})
